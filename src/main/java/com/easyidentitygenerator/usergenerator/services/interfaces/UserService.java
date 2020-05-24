2917393636f6759fc7a86002cd87d8a18ddfffd5
Nicolas Viana da Silva <nicolas.viana.silva@outlook.com.br>
2020-05-23 22:23:48 -0300

    fix get users

--- src/main/java/com/easyidentitygenerator/usergenerator/services/interfaces/UserService.java
+++ src/main/java/com/easyidentitygenerator/usergenerator/services/interfaces/UserService.java
@@ -3,12 +3,13 @@ package com.easyidentitygenerator.usergenerator.services.interfaces;
 import com.easyidentitygenerator.usergenerator.entities.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name= "users", url = "https://randomuser.me/api")
+@FeignClient(name= "users", url = "https://randomuser.me/api/?nat=br&inc=login,email")
 @Service
 public interface UserService {
-    @RequestMapping(method = RequestMethod.GET, value = "/?nat=br&inc=login,email")
+    @GetMapping
     User getUser();
 }
